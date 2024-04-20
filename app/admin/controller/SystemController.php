732a56f73694fa192187f15baf23fe66cc9e343f
david <nz1985@qq.com>
2024-04-20 12:48:56 +0800

    fix

--- app/admin/controller/SystemController.php
+++ app/admin/controller/SystemController.php
@@ -2,6 +2,8 @@
 
 namespace app\admin\controller;
 
+use app\common\HasJsonResponse;
+use Iyuu\SiteManager\Spider\Params;
 use support\Request;
 use support\Response;
 
@@ -10,6 +12,8 @@ use support\Response;
  */
 class SystemController
 {
+    use HasJsonResponse;
+
     /**
      * 执行操作
      * - 启动、重启、停止、重载等webman支持的命令
@@ -19,6 +23,10 @@ class SystemController
     public function action(Request $request): Response
     {
         $command = $request->post('command', 'restart');
+        if (!in_array($command, Params::TYPE_LIST, true)) {
+            return $this->fail('不受支持的命令，允许：' . implode('|', Params::TYPE_LIST));
+        }
+
         $cmd = implode(' ', ['php', base_path('start.php'), $command]);
         exec($cmd);
         sleep(3);
