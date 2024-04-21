60a21cbf9f4f17a91f83380bdf9c5808c2e030c7
david <nz1985@qq.com>
2024-04-21 12:00:35 +0800

    优化：异常处理

--- app/admin/services/SitesServices.php
+++ app/admin/services/SitesServices.php
@@ -4,6 +4,7 @@ namespace app\admin\services;
 
 use app\model\Site;
 use Iyuu\ReseedClient\Client;
+use Throwable;
 
 /**
  * 站点服务层
@@ -16,25 +17,28 @@ class SitesServices
      */
     public static function sync(): void
     {
-        $reseedClient = new Client(iyuu_token());
-        $list = $reseedClient->sites();
-        file_put_contents(runtime_path('sync.json'), json_encode($list, JSON_UNESCAPED_UNICODE));
-        foreach ($list as $site => $item) {
-            $siteModel = Site::uniqueSite($site);
-            if (!$siteModel) {
-                $siteModel = new Site();
-                $siteModel->sid = $item['id'];
-                $siteModel->site = $site;
-            }
+        try {
+            $reseedClient = new Client(iyuu_token());
+            $list = $reseedClient->sites();
+            file_put_contents(runtime_path('sync.json'), json_encode($list, JSON_UNESCAPED_UNICODE));
+            foreach ($list as $site => $item) {
+                $siteModel = Site::uniqueSite($site);
+                if (!$siteModel) {
+                    $siteModel = new Site();
+                    $siteModel->sid = $item['id'];
+                    $siteModel->site = $site;
+                }
 
-            $siteModel->nickname = $item['nickname'];
-            $siteModel->base_url = $item['base_url'];
-            $siteModel->download_page = $item['download_page'] ?? '';
-            $siteModel->details_page = $item['details_page'] ?? '';
-            $siteModel->reseed_check = $item['reseed_check'] ?? '';
-            $siteModel->is_https = $item['is_https'] ?? 1;
-            $siteModel->cookie_required = $item['cookie_required'] ?? 0;
-            $siteModel->save();
+                $siteModel->nickname = $item['nickname'];
+                $siteModel->base_url = $item['base_url'];
+                $siteModel->download_page = $item['download_page'] ?? '';
+                $siteModel->details_page = $item['details_page'] ?? '';
+                $siteModel->reseed_check = $item['reseed_check'] ?? '';
+                $siteModel->is_https = $item['is_https'] ?? 1;
+                $siteModel->cookie_required = $item['cookie_required'] ?? 0;
+                $siteModel->save();
+            }
+        } catch (Throwable $throwable) {
         }
     }
 }
