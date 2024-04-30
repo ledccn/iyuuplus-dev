f39d41d695a91320c713348b8efea170dde40528
david <367013672@qq.com>
2024-04-30 12:00:06 +0800

    新增：花梨月下配置

--- app/admin/services/site/LayuiTemplate.php
+++ app/admin/services/site/LayuiTemplate.php
@@ -90,6 +90,7 @@ class LayuiTemplate
             'ilolicon',
             'gtkpw',
             'icc2022',
+            'ecustpt',
             'keepfrds' => Decorator::make([NexusPHP::class, OptionsUrlJoin::class], $default),
             'ttg' => Decorator::make([NexusPHP::class, OptionsLimit::class, OptionsRssUrl::class], $default),
             'redleaves', 'pter', 'pt', 'hdsky' => Decorator::make([NexusPHP::class, OptionsLimit::class], $default),
