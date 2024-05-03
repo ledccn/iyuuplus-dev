f146f4c28919655ef8009d69c5533fc1a7d6546a
david <nz1985@qq.com>
2024-05-03 20:08:08 +0800

    适配：悟空、蟹黄堡

---
+++ composer/site-manager/src/Driver/DriverCrabpt.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Iyuu\SiteManager\Driver;
+
+use Iyuu\SiteManager\Contracts\Processor;
+use Iyuu\SiteManager\Contracts\ProcessorXml;
+use Iyuu\SiteManager\Frameworks\NexusPhp\HasRss;
+use Iyuu\SiteManager\BaseDriver;
+
+/**
+ * crabpt
+ */
+class DriverCrabpt extends BaseDriver implements Processor, ProcessorXml
+{
+    use HasRss;
+    /**
+     * 站点名称
+     */
+    public const SITE_NAME = 'crabpt';
+}
