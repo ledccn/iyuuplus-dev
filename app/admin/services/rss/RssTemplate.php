c822573164df4ab81cb316cfbc408c7c49eb4bb6
david <367013672@qq.com>
2024-07-05 11:59:57 +0800

    新增：RSS订阅任务类型（未完成）

--- app/admin/services/rss/RssTemplate.php
+++ app/admin/services/rss/RssTemplate.php
@@ -50,13 +50,24 @@ class RssTemplate extends CrontabAbstract
         <input type="text" name="target" value="$command" required lay-verify="required" placeholder="请输入命令名称" class="layui-input" readonly>
     </div>
 </div>
+
 <div name="parameter" id="parameter" value="" class="layui-hide"></div>
+
+<div class="layui-form-item">
+    <label class="layui-form-label required">RSS地址</label>
+    <div class="layui-input-block">
+        <input type="text" name="parameter[rss_url]" value="" required lay-verify="required" placeholder=""
+               class="layui-input">
+    </div>
+</div>
+
 <div class="layui-form-item">
     <label class="layui-form-label required">下载器</label>
     <div class="layui-input-block">
         <div name="parameter[client_id]" id="client_id" value=""></div>
     </div>
 </div>
+
 <div class="layui-form-item">
     <label class="layui-form-label required" title="添加下载任务时，对种子做标记">标记规则</label>
     <div class="layui-input-block">
@@ -66,6 +77,89 @@ class RssTemplate extends CrontabAbstract
     </div>
     <div class="layui-form-mid layui-text-em">添加下载任务时，对种子做标记（需要下载器支持）</div>
 </div>
+
+<div class="layui-form-item">
+    <label class="layui-form-label">保存路径</label>
+    <div class="layui-input-block">
+        <input type="text" name="parameter[save_path]" value="" placeholder="如果留空，依次使用：资源文件夹 -> 下载器默认" autocomplete="off" class="layui-input">
+    </div>
+</div>
+
+<div class="layui-form-item">
+    <div class="layui-inline">
+      <label class="layui-form-label">种子大小</label>
+      <div class="layui-input-inline">
+        <input type="number" name="parameter[size_min]" placeholder="最小值" autocomplete="off" class="layui-input">
+      </div>
+      <div class="layui-input-inline" style="width: 120px;">
+        <select name="parameter[size_min_unit]">
+          <option value="">最小值单位</option>
+          <option value="KB">KB</option>
+          <option value="MB">MB</option>
+          <option value="GB">GB</option>
+          <option value="TB">TB</option>
+        </select>
+      </div>
+      <div class="layui-form-mid">-</div>
+      <div class="layui-input-inline">
+        <input type="number" name="parameter[size_max]" placeholder="最大值" autocomplete="off" class="layui-input">
+      </div>
+      <div class="layui-input-inline" style="width: 120px;">
+        <select name="parameter[size_max_unit]">
+          <option value="">最大值单位</option>
+          <option value="KB">KB</option>
+          <option value="MB">MB</option>
+          <option value="GB">GB</option>
+          <option value="TB">TB</option>
+        </select>
+      </div>
+    </div>
+</div>
+
+<div class="layui-form-item">
+    <label class="layui-form-label">规则设置</label>
+    <div class="layui-input-block">
+        <div class="layui-collapse" lay-accordion>
+          <div class="layui-colla-item">
+            <div class="layui-colla-title">简易模式</div>
+            <div class="layui-colla-content layui-show">
+                <div class="layui-form-item">
+                    <label class="layui-form-label">包含关键字</label>
+                    <div class="layui-input-block">
+                        <div class="layui-btn-container tag" lay-filter="tag_selector" lay-allowclose="true" lay-newTag="true">
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">排除关键字</label>
+                    <div class="layui-input-block">
+                        <div class="layui-btn-container tag" lay-filter="tag_filter" lay-allowclose="true" lay-newTag="true">
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="layui-colla-item">
+            <div class="layui-colla-title">正则模式</div>
+            <div class="layui-colla-content">
+                <div class="layui-form-item">
+                    <label class="layui-form-label">选中规则</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="parameter[rule_selector]" value="" placeholder="请输入正则表达式" autocomplete="off" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">排除规则</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="parameter[rule_filter]" value="" placeholder="请输入正则表达式" autocomplete="off" class="layui-input">
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
+
 EOF;
     }
 
--- app/admin/services/rss/rss.js
+++ app/admin/services/rss/rss.js
@@ -31,6 +31,67 @@ update_render_callable.push(
 
                 }
             });
+        })
+
+        layui.use(['tag', 'element'], function() {
+            let $ = layui.jquery,
+                tag = layui.tag; //Tag的切换功能，切换事件监听等，需要依赖tag模块
+
+            tag.render("tag_selector", {
+                skin: 'layui-btn layui-btn-primary layui-btn-sm layui-btn-radius', //标签样式
+                tagText: '<i class="layui-icon layui-icon-add-1"></i>添加' //标签添加按钮提示文本
+            });
+
+            tag.on('click(tag_selector)', function(data) {
+                console.log('点击');
+                console.log(this); //当前Tag标签所在的原始DOM元素
+                console.log(data.index); //得到当前Tag的所在下标
+                console.log(data.elem); //得到当前的Tag大容器
+            });
+
+            tag.on('add(tag_selector)', function(data) {
+                console.log('新增');
+                console.log(this); //当前Tag标签所在的原始DOM元素
+                console.log(data.index); //得到当前Tag的所在下标
+                console.log(data.elem); //得到当前的Tag大容器
+                console.log(data.othis); //得到新增的DOM对象
+                //return false; //返回false 取消新增操作； 同from表达提交事件。
+            });
+
+            tag.on('delete(tag_selector)', function(data) {
+                console.log('删除');
+                console.log(this); //当前Tag标签所在的原始DOM元素
+                console.log(data.index); //得到当前Tag的所在下标
+                console.log(data.elem); //得到当前的Tag大容器
+            });
+
+            tag.render("tag_filter", {
+                skin: 'layui-btn tag-item-normal layui-btn-primary layui-btn-sm layui-btn-radius', //标签样式
+                tagText: '<i class="layui-icon layui-icon-add-1"></i>添加' //标签添加按钮提示文本
+            });
+
+            tag.on('click(tag_filter)', function(data) {
+                console.log('点击');
+                console.log(this); //当前Tag标签所在的原始DOM元素
+                console.log(data.index); //得到当前Tag的所在下标
+                console.log(data.elem); //得到当前的Tag大容器
+            });
+
+            tag.on('add(tag_filter)', function(data) {
+                console.log('新增');
+                console.log(this); //当前Tag标签所在的原始DOM元素
+                console.log(data.index); //得到当前Tag的所在下标
+                console.log(data.elem); //得到当前的Tag大容器
+                console.log(data.othis); //得到新增的DOM对象
+                //return false; //返回false 取消新增操作； 同from表达提交事件。
+            });
+
+            tag.on('delete(tag_filter)', function(data) {
+                console.log('删除');
+                console.log(this); //当前Tag标签所在的原始DOM元素
+                console.log(data.index); //得到当前Tag的所在下标
+                console.log(data.elem); //得到当前的Tag大容器
+            });
         });
     }
 );
