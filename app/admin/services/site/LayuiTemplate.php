a5823c6c07deb7615258b6e12cdcbc78e7345367
david <nz1985@qq.com>
2024-12-29 19:41:08 +0800

    新增：站点 kufei 库非

--- app/admin/services/site/LayuiTemplate.php
+++ app/admin/services/site/LayuiTemplate.php
@@ -100,6 +100,7 @@ class LayuiTemplate
             'njtupt',
             'ptzone',
             'hdclone',
+            'kufei',
             'keepfrds' => Decorator::make([NexusPHP::class, OptionsUrlJoin::class], $default),
             'yemapt' => Decorator::make([OptionsRssUrl::class], $default),
             'ttg' => Decorator::make([NexusPHP::class, OptionsLimit::class, OptionsRssUrl::class], $default),
