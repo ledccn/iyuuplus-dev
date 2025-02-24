4081b56b4882aac9429a8099710980588f355e1b
david <367013672@qq.com>
2025-02-24 15:04:01 +0800

    新增：回声站点

--- app/admin/services/site/LayuiTemplate.php
+++ app/admin/services/site/LayuiTemplate.php
@@ -101,6 +101,7 @@ class LayuiTemplate
             'ptzone',
             'hdclone',
             'kufei',
+            'hspt',
             'keepfrds' => Decorator::make([NexusPHP::class, OptionsUrlJoin::class], $default),
             'yemapt' => Decorator::make([OptionsRssUrl::class], $default),
             'ttg' => Decorator::make([NexusPHP::class, OptionsLimit::class, OptionsRssUrl::class], $default),
---
+++ composer/site-manager/src/Cookie/CookieHspt.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Iyuu\SiteManager\Cookie;
+
+use Iyuu\SiteManager\BaseCookie;
+use Iyuu\SiteManager\Frameworks\NexusPhp\HasCookie;
+use Iyuu\SiteManager\Spider\Pagination;
+
+/**
+ * hspt
+ * - 凭cookie解析HTML列表页
+ */
+class CookieHspt extends BaseCookie
+{
+    use HasCookie, Pagination;
+
+    /**
+     * 站点名称
+     */
+    public const string SITE_NAME = 'hspt';
+
+    /**
+     * 是否调试当前站点
+     * @return bool
+     */
+    protected function isDebugCurrent(): bool
+    {
+        return false;
+    }
+}
