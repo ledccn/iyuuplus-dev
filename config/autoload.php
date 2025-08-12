283fe57103c92ac579cd0b8df70e9267e963ed60
david <367013672@qq.com>
2025-08-12 09:56:29 +0800

    调整：移除未使用的类

--- config/autoload.php
+++ config/autoload.php
@@ -6,8 +6,6 @@
 return [
     'files' => [
         base_path() . '/app/functions.php',
-        base_path() . '/support/Cache.php',
-        base_path() . '/support/Model.php',
         base_path() . '/support/Request.php',
     ]
 ];
